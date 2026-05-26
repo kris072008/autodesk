ce5ce26701499fae1b61963dc56ebc761a997859
kris072008 <krishnamaha12300@gmail.com>
2026-05-26 14:21:57 +0800

    keyboard

---
+++ -- Layout.lua
@@ -0,0 +1,50 @@
+-- Layout
+key_pitch          = 19.05 mm        (MX centre-to-centre)
+key_u              = 19.05 mm        (1U reference)
+switch_cutout      = 14.00 mm        (MX plate cutout)
+switch_snap        = 1.50 mm         (plate snap-fit depth)
+stab_2u_centre     = 23.80 mm        (centre-to-centre for 2U stab)
+stab_spacebar      = 100.00 mm       (6.25U spacebar stab spread)
+
+-- Case geometry
+case_length        = 327.00 mm
+case_width         = 113.00 mm
+case_height_top    =  18.50 mm
+case_height_bottom =  16.50 mm
+total_height       =  35.00 mm       (without feet)
+typing_angle       =   7.00 deg
+corner_radius      =   6.00 mm
+wall_thickness     =   2.50 mm
+bezel_front        =   4.50 mm
+bezel_sides        =   4.50 mm
+bezel_rear         =   6.00 mm
+
+-- Internal clearances
+pcb_thickness      =   1.60 mm
+plate_thickness    =   1.50 mm
+foam_thickness     =   2.00 mm
+switch_height_pcb  =   6.60 mm       (MX switch PCB to top of housing)
+keycap_travel      =   4.00 mm
+keycap_top_height  =   8.00 mm       (SA row 3 reference)
+plate_to_top_case  =  16.00 mm       (clearance for tallest keycap)
+
+-- Hardware
+screw_od           =   3.00 mm       (M3 screws)
+screw_head_od      =   5.50 mm       (M3 countersink)
+insert_od          =   4.50 mm       (M3 heat-set insert OD)
+insert_depth       =   5.00 mm
+gasket_thickness   =   1.50 mm
+gasket_width       =   4.00 mm
+usbc_width         =   9.00 mm
+usbc_height        =   3.20 mm
+knob_diameter      =  17.00 mm
+knob_height        =  14.00 mm
+oled_width         =  26.00 mm
+oled_height        =  14.00 mm
+rgb_channel_depth  =   1.80 mm
+rgb_channel_width  =   3.00 mm
+foot_recess_depth  =   2.00 mm
+foot_recess_dia    =  16.00 mm
+battery_length     =  60.00 mm
+battery_width      =  35.00 mm
+battery_height     =  10.00 mm
